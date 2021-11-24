af0bf5f482593341b79b71af0a34ba5cd73ee86b
mharizanova8703 <85656320+mharizanova8703@users.noreply.github.com>
2021-11-24 14:05:21 -0500

    Update tag-routes.js
    creating  router  delete for tags if , we request a wrong tag id is going to pop a note 404, if the tag which we want to delete exit  is 200 (ok)

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -56,8 +56,23 @@ router.put('/:id', async (req, res) => {
   }
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+
+    if (!deleteTag) {
+      res.status(404).json({ message: 'No tag found with this id!' })
+      return
+    }
+    res.status(200).json(deleteTag)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
 
 module.exports = router
